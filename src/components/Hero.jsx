cc57173ada0df47cf84d47f52a150b997247027a
Vovababiichuk <babihuk888@gmail.com>
2023-11-05 17:55:13 +0200

    header....

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ export const Hero = () => {
 			<div className='container mx-auto h-full flex justify-center items-center xl:justify-start'>
 				{/* text */}
 				<div className='h-full flex flex-col justify-center items-center xl:items-start z-20 pt-12'>
-					<MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.2} resetOnLeave className='relative flex items-center p-[120px] xl:h-max xl:w-[840px]'>
+					<MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.2} resetOnLeave className='relative flex items-center xl:h-max xl:w-[840px]'>
 						<MouseParallaxChild factorX={0.2} factorY={0.4} className='relative'>
 							<motion.div
 								variants={fadeIn('up', 0.4)}
@@ -48,12 +48,32 @@ export const Hero = () => {
 								whileInView={"show"}
 								viewport={{ once: false, amount: 0.3 }}
 								className='w-[150px] h-[100px] xl:w-[248px] xl:h-[200px] mix-blend-luminosity relative'>
-								<Image src={'/assets/hero/bird.png'} priority fill alt='' className='object-contain' />
+								<Image src={'/assets/hero/bird.png'} priority fill alt='' sizes='(100vw - 100px)' className='object-contain' />
 							</motion.div>
 						</MouseParallaxChild>
 					</MouseParallaxContainer>
 
-					
+					<div className='min-h-[60px] flex items-center mb-6'>
+						<div className='hidden xl:flex items-center xl:gap-x-0'>
+							<div>World</div>
+							<div className='relative w-2 h-2 mx-2 flex items-center justify-center'>
+								<Image src={'/assets/hero/dot.svg'} fill alt=''/>
+							</div>
+						</div>
+						{/* icon */}
+						<div className='hidden xl:flex items-center justify-center relative w-7 h-7 mx-4'>
+							<Image src={'/assets/hero/mic.svg'} priority fill alt='' />
+						</div>
+						{/* animation */}
+						<TypeAnimation
+							sequence={locationSequence}
+							wrapper='div'
+							speed={10}
+							deletionSpeed={10}
+							repeat={Infinity}
+							cursor={false} />
+					</div>
+
 				</div>
 				{/* image */}
 				<motion.div
