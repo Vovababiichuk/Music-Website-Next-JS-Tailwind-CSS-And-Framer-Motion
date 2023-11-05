5cbd6b8f65dc67b33864eb50107f9c9c98c786e8
Vovababiichuk <babihuk888@gmail.com>
2023-11-05 12:18:41 +0200

    fix box scroll

--- src/components/events/EventBox.jsx
+++ src/components/events/EventBox.jsx
@@ -4,7 +4,7 @@ import { RiMapPin2Fill } from 'react-icons/ri';
 export const EventBox = ({ events }) => {
 	return (
 		<div className="bg-secondary/60 rounded-[10px] p-4 xl:p-12 relative">
-			<div className="flex flex-co xl:flex-row justify-between h-[620px] xl:h-full gap-x-4">
+			<div className="flex flex-col xl:flex-row justify-between h-[620px] xl:h-full gap-x-4">
 				<div className='hidden xl:flex w-[400px]'>
 					<Image src={'/assets/events/singer.png'} width={358} height={489} priority quality={100} alt='' className="w-auto h-auto" />
 				</div>
