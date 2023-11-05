d76d6a1246148518c3e24d98712e42d1bf621cbc
Vovababiichuk <babihuk888@gmail.com>
2023-11-05 14:28:43 +0200

    finish Events Section

--- src/components/events/EventBox.jsx
+++ src/components/events/EventBox.jsx
@@ -8,11 +8,11 @@ import { fadeIn } from '../../../variants'
 export const EventBox = ({ events }) => {
 	return (
 		<motion.div
-		variants={fadeIn('up', 0.4)}
-		initial="hidden"
-		whileInView={"show"}
-		viewport={{ once: false, amount: 0.3 }}
-		className="bg-secondary/60 rounded-[10px] p-4 xl:p-12 relative">
+			variants={fadeIn('up', 0.6)}
+			initial="hidden"
+			whileInView={"show"}
+			viewport={{ once: false, amount: 0.3 }}
+			className="bg-secondary/60 rounded-[10px] p-4 xl:p-12 relative">
 			<div className="flex flex-col xl:flex-row justify-between h-[620px] xl:h-full gap-x-4">
 				<div className='hidden xl:flex w-[400px]'>
 					<Image src={'/assets/events/singer.png'} width={358} height={489} priority quality={100} alt='' className="w-auto h-auto" />
--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,4 +1,5 @@
 import { EventBox } from "./EventBox";
+import { SectionHeader } from "./SectionHeader";
 
 const getEvents = async () => {
 	const res = await fetch(' http://localhost:4000/events')
@@ -11,6 +12,8 @@ export const Events = async () => {
 	return (
 		<section className="section" id='tours'>
 			<div className="container mx-auto">
+				<SectionHeader pretitle="World Tour" title="Upcoming Events" />
+				{/* event box */}
 				<EventBox events={events} />
 			</div>
 		</section>
