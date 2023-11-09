50892102d1a5b159563826994876c7ea3e4bec88
Vovababiichuk <babihuk888@gmail.com>
2023-11-09 11:31:00 +0200

    fix position close icon mobile menu

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -12,7 +12,7 @@ export const NavMobile = () => {
 		>
 			<div
 				onClick={() => setIsOpen(false)}
-				className="absolute right-[12px] top-5 cursor-pointer">
+				className="absolute right-4 top-5 cursor-pointer">
 				<RiCloseLine className="text-5xl" />
 			</div>
 			<Nav containerStyles='flex flex-col text-[30px] uppercase font-bold bg-pink-500/10 h-full items-center justify-center gap-y-8' />
