992150c89e238efed57c775a550d42f61ad72aab
Vovababiichuk <babihuk888@gmail.com>
2023-11-10 10:35:28 +0200

    added animation Album Section

--- src/components/albums/Albums.jsx
+++ src/components/albums/Albums.jsx
@@ -12,9 +12,14 @@ export const Albums = () => {
 			<div className="container mx-auto">
 				<SectionHeader pretitle="Discography" title="Popular Albums" />
 				{/* album slider */}
-				<div>
+				<motion.div
+					variants={fadeIn('up', 0.4)}
+					initial="hidden"
+					whileInView={"show"}
+					viewport={{ once: false, amount: 0.3 }}
+				>
 					<AlbumSlider />
-				</div>
+				</motion.div>
 			</div>
 		</section>
 	)
