6f05ea2275f6589415386a74e87948a96a839dde
Vovababiichuk <babihuk888@gmail.com>
2023-11-11 12:54:36 +0200

    finish Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,28 +9,48 @@ import { fadeIn } from "../../variants"
 
 export const Footer = () => {
 	return (
-		<footer className="bg-accent section">
-			<div className="container mx-auto flex flex-col items-center gap-y-8">
+		<footer className="bg-accent section h-[500px] xl:h-auto">
+			<div className="container mx-auto h-full flex flex-col items-center gap-y-8">
 				{/* email link */}
-				<div>
+				<motion.div
+					variants={fadeIn('up', 0.2)}
+					initial='hidden'
+					whileInView={"show"}
+					viewport={{ once: false, amount: 0.3 }}
+				>
 					<Link href='#'>
 						<h2 className="text-[24px] lg:text-[38px] font-semibold leading-tight">hello@miareynolds.com</h2>
 					</Link>
-				</div>
+				</motion.div>
 				{/* nav */}
-				<div>
+				<motion.div
+					variants={fadeIn('up', 0.4)}
+					initial='hidden'
+					whileInView={"show"}
+					viewport={{ once: false, amount: 0.3 }}
+				>
 					<Nav containerStyles='flex flex-col xl:flex-row justify-center items-center gap-y-4 xl:gap-x-8 text-sm uppercase font-semibold' linkStyles='hover:text-primary/80 transition-all' />
-				</div>
+				</motion.div>
 				{/* socials */}
-				<div>
+				<motion.div
+					variants={fadeIn('up', 0.6)}
+					initial='hidden'
+					whileInView={"show"}
+					viewport={{ once: false, amount: 0.3 }}
+				>
 					<Socials containerStyles='flex text-[24px] gap-x-8' iconStyles='hover:text-primary/80 transition-all justify-center' />
-				</div>
+				</motion.div>
 				{/* logo */}
-				<div>
-					<Link href='#' className="relative w-[250px] h-[50px] flex transition-all mb-4 xl:mb-0">
-						<Image src={'/assets/header/logo.svg'} fill priority alt='' className='object-contain'/>
+				<motion.div
+					variants={fadeIn('up', 0.8)}
+					initial='hidden'
+					whileInView={"show"}
+					viewport={{ once: false, amount: 0.1}}
+				>
+					<Link href='#' className="relative w-[250px] h-[50px] flex transition-all">
+						<Image src={'/assets/header/logo.svg'} fill priority alt='' className='object-contain' />
 					</Link>
-				</div>
+				</motion.div>
 			</div>
 		</footer>
 	)
